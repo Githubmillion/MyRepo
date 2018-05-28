29682e0845b521e11c55b0ec3aa8a1de4ef09dd5
Himaja Yenikepati <himaja.yenikepati@gmail.com>
2018-05-28 19:05:26 -0400

    Adding deploy and parameter files

---
+++ IIS_Deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+export AWS_CREDENTIALS=`aws sts assume-role --role-arn arn:aws:iam::217912576673:role --role-session-name "$(curl 169.254.169.254/latest/meta-data/instance-id)-$(date +%s)" --output text`
+export AWS_ACCESS_KEY_ID=`echo $AWS_CREDENTIALS | cut -d ' ' -f 5`
+export AWS_SECRET_ACCESS_KEY=`echo $AWS_CREDENTIALS | cut -d ' ' -f 7`
+export AWS_SESSION_TOKEN=`echo $AWS_CREDENTIALS | cut -d ' ' -f 8`
+export AWS_DEFAULT_REGION=${Region}
+
+
+# if [ ${Environment} == "dev" ]; then
+#     ENVIRONMENTNAME="nonprod"
+# else
+#     ENVIRONMENTNAME="preprod"
+# fi
+export STACK_NAME="DemoIIS-${Environment}"
+   aws cloudformation create-stack --region ${Region} --stack-name ${STACK_NAME} --template-body file://`pwd`/IIS_CFT.json --parameters file://`pwd`/IIS_Params.json
+   aws cloudformation wait stack-create-complete --stack-name ${STACK_NAME}
