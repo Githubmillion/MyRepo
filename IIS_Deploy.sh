e91a54c6a0527dc52db44b3f486739ba802ca4b0
Himaja Yenikepati <himaja.yenikepati@gmail.com>
2019-03-26 10:23:18 -0400

    Update IIS_Deploy.sh

--- IIS_Deploy.sh
+++ IIS_Deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export AWS_CREDENTIALS=`aws sts assume-role --role-arn arn:aws:iam::217912576673:role/assumerole --role-session-name --profile Himaja "$(curl 169.254.169.254/latest/meta-data/instance-id)-$(date +%s)" --output text`
+export AWS_CREDENTIALS=`aws sts assume-role --role-arn arn:aws:iam::217912576673:role/Myaccount_custompolicy --role-session-name --profile Himaja "$(curl 169.254.169.254/latest/meta-data/instance-id)-$(date +%s)" --output text`
 export AWS_ACCESS_KEY_ID=`echo $AWS_CREDENTIALS | cut -d ' ' -f 5`
 export AWS_SECRET_ACCESS_KEY=`echo $AWS_CREDENTIALS | cut -d ' ' -f 7`
 export AWS_SESSION_TOKEN=`echo $AWS_CREDENTIALS | cut -d ' ' -f 8`
